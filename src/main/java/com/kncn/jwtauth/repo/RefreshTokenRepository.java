113340ca06bc91742c474145a334734ea0c21b4e
Kaan Olcay <71110056+KNCn23@users.noreply.github.com>
2026-05-21 12:17:58 +0300

    Add src/main/java/com/kncn/jwtauth/repo/RefreshTokenRepository.java

---
+++ src/main/java/com/kncn/jwtauth/repo/RefreshTokenRepository.java
@@ -0,0 +1,16 @@
+package com.kncn.jwtauth.repo;
+
+import com.kncn.jwtauth.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+
+    @Modifying
+    @Query("UPDATE RefreshToken r SET r.revoked = true WHERE r.user.id = :uid")
+    void revokeAllForUser(@Param("uid") Long userId);
+}
